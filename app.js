e464f530f5f49fff13dd780bd9834d4d344ea7ba
Sergejus <sergejus.barinovas@adform.com>
2013-02-05 17:09:04 +0200

    app bones

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon(__dirname + '/favicon.ico'));
+  app.use(express.compress());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+});
+
+http.createServer(app).listen(app.get('port'));
