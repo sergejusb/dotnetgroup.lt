c90912a8cd717b31f48b92a85071ce72585e2f21
Sergejus <seriog@yahoo.com>
2013-02-05 22:28:04 +0200

    added views

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.configure(function() {
   app.set("port", process.env.PORT || 3000);
   app.set("views", __dirname + "/views");
   app.set("view engine", "jade");
-  app.use(express.favicon(__dirname + '/favicon.ico'));
+  app.use(express.favicon(__dirname + "/favicon.ico"));
   app.use(express.compress());
   app.use(express.cookieParser());
   app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/public'));
+  app.use("/public", express.static(__dirname + "/public"));
 });
 
 app.locals.title = "Lietuvos .net naudotojų grupė";
