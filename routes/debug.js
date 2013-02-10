f7da9e3bad631acc226a9839b87e66603ee68d3f
Sergejus <seriog@yahoo.com>
2013-02-10 13:03:48 +0200

    removed tabs

--- routes/debug.js
+++ routes/debug.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   return function(req, res) {
-  	var info = {
+    var info = {
       "env" : app.get("env"),
       "events" : require('../models/events.js')
     };
